05237e974a5371bf1bb86359f91a99ba6c166631
LeeeLiu <329742300@qq.com>
2019-12-24 20:50:10 +0800

    update

---
+++ batch_process.bat
@@ -0,0 +1,11 @@
+
+
+
+
+lt_emb_STC   4000_Qmdct_cover\00001.wav.txt   4000_pro\00001.wav.txt   4000_message\1.txt   0.1
+embed_STC    -o  aac_file\00001-stego.aac     wav_file\00001.wav
+2-channel-extract_STC    0.01827\aac_file\00002.wav.m4a
+lt_extr_STC    de_Sqmdct.txt   n_msg_bits.txt   4000_extr_msg\1.txt
+
+rename   de_Sqmdct.txt    00001.wav.txt
+move     00001.wav.txt  .\4000_Qmdct_stego
---
+++ batch_stego_generate.bat
@@ -0,0 +1,9 @@
+
+
+
+
+lt_emb_STC   2000_Qmdct_cover\00024.wavQmdct_cover.txt   1500_pro\00024.wavQmdct_cover.txt   1500_message\24.txt   0.1
+embed_STC    -o  aac_file\00024-stego.aac     wav_file\00024.wav
+
+rename   Sqmdct.txt    00024.wavQmdct_cover.txt
+move     00024.wavQmdct_cover.txt  .\1500_Qmdct_stego
